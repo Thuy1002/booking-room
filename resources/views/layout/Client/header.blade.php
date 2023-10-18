36c38d9852adc0d6349a7f34222e62c3a9d8b005
thuy1002 <thuy1002dangthanh@gmail.com>
2023-10-19 00:58:55 +0700

    update layout client

---
+++ resources/views/layout/Client/header.blade.php
@@ -0,0 +1,63 @@
+<div class="">
+    <nav class="navbar navbar-expand-lg navbar-light">
+
+        <a class="navbar-brand logo_h" href="index.html"><img src="{{ asset('client/image/Logo.png') }}" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+        <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav ml-auto">
+                <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.html">About us</a></li>
+                <li class="nav-item"><a class="nav-link" href="accomodation.html">Accomodation</a></li>
+                <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
+                <li class="nav-item submenu dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                        aria-haspopup="true" aria-expanded="false">Blog</a>
+                    <ul class="dropdown-menu">
+                        <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                        <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog Details</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="elements.html">Elemests</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+        <nav style="border-radius:10px;margin-left:20px" class="navbar navbar-expand-lg navbar-light bg-light">
+            <!-- Dropdown Menu -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li  class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img style=" width: 35px;"
+                                src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt="">
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Profile</a>
+                            <a class="dropdown-item" href="#">Settings</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Logout</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="roomsDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Phòng đã đặt
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="roomsDropdown">
+                            <a class="dropdown-item" href="#">Phòng 1</a>
+                            <a class="dropdown-item" href="#">Phòng 2</a>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+
+    </nav>
+</div>
---
+++ resources/views/layout/Client/master.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="image/favicon.png" type="image/png">
+    <title>@yield('title')</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('client/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/vendors/linericon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/vendors/owl-carousel/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/vendors/nice-select/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/vendors/owl-carousel/owl.carousel.min.css') }}">
+    <!-- main css -->
+    <link rel="stylesheet" href="{{ asset('client/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/css/responsive.css') }}">
+</head>
+
+<body>
+    <!--================Header Area =================-->
+    <header class="header_area">
+        @include('layout.Client.header')
+    </header>
+    <!--================Header Area =================-->
+
+    <!--================Banner Area =================-->
+    <section class="banner_area">
+        @include('layout.Client.banner')
+    </section>
+    <!--================Banner Area =================-->
+
+    <!--================ Accomodation Area  =================-->
+    @yield('content')
+    <!--================ Recent Area  =================-->
+
+    <!--================ start footer Area  =================-->
+    <footer class="footer-area section_gap">
+        @include('layout.Client.footer')
+    </footer>
+    <!--================ End footer Area  =================-->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{ asset('client/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('client/js/popper.js') }}"></script>
+    <script src="{{ asset('client/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('client/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('client/js/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('client/js/mail-script.js') }}"></script>
+    <script src="{{ asset('client/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js') }}"></script>
+    <script src="{{ asset('client/vendors/nice-select/js/jquery.nice-select.js') }}"></script>
+    <script src="{{ asset('client/js/mail-script.js') }}"></script>
+    <script src="{{ asset('client/js/stellar.js') }}"></script>
+    <script src="{{ asset('client/vendors/lightbox/simpleLightbox.min.js') }}"></script>
+    <script src="{{ asset('client/js/custom.js') }}"></script>
+</body>
+
+</html>
