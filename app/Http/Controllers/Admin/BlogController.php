695a92acecd88004a2d3b2cd329ff50b114b3e98
thuy1002 <thuy1002dangthanh@gmail.com>
2024-07-11 23:51:46 +0700

    update code ql blog

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BlogController extends Controller
 {
-    //
+    public function index(){
+        $user = Auth::user();
+        $blog = blog::with(['user','images'])->orderBy('created_at','desc')->paginate(4);
+        return view('Admin.blog.list',compact('user','blog'));
+    }
+    public function updateStatus(Request $request, $id)
+    {
+        $item = blog::find($id);
+        $item->status = $request->input('status');
+        $item->save();
+        return response()->json(['success' => 'Cập nhật trạng thái thành công']);
+    }
+    public function delet($id)
+    {
+        $item = blog::find($id);
+        $item->delete();
+        return back()->with('success', 'Xóa thành công');
+    }
 }
